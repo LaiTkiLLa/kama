5df644beb5914b9fd9cfc311fd7299095281a4dc
Fedor Burov <f.burov@kubis.ru>
2025-04-14 18:16:49 +0300

    СДелал запросы на получение товаров ЯНдекс, добавил поле в таблице, почти реализовал получение остатков по яндекс

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cron } from '@nestjs/schedule';
 import { DataSource, FindOptionsWhere, QueryRunner } from 'typeorm';
 import axios from 'axios';
 import { InfoService } from '../info/info.service';
