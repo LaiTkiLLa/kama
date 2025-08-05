bdee191d5e6c931e1bce43937b78d52b7e6813d9
Fedor Burov <f.burov@kubis.ru>
2025-08-05 07:27:43 +0300

    little fixes

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -388,7 +388,8 @@ export class ItemsService {
     return;
   }
 
-  @Cron('0 */44 * * * *')
+  // @Cron('0 */44 * * * *')
+  @Cron(CronExpression.EVERY_MINUTE)
   async getYandexItems() {
     const businessId = await this.configService.get('yandexBusinessId');
     const itemsUrl = `https://api.partner.market.yandex.ru/businesses/${businessId}/offer-mappings?limit=200`;
@@ -403,6 +404,7 @@ export class ItemsService {
     );
     const yandexMarketplace = await this.infoService.findMarketplace({ title: 'Yandex' });
     for (const item of data.result.offerMappings) {
+      console.log(item);
       const queryRunner = await this.dataSource.createQueryRunner();
       await queryRunner.connect();
       try {
@@ -589,9 +591,9 @@ export class ItemsService {
             { id: item.itemId },
             { sendStatusId: findRecommendedStatus.id }
           );
-        } else if (result <= 0) {
+        } else if (item.stocks - item.orders <= 0) {
           await queryRunner.manager.update(Items, { id: item.itemId }, { sendStatusId: findRejectStatus.id });
-        } else {
+        } else if (item.stocks - item.orders > 0) {
           await queryRunner.manager.update(
             Items,
             { id: item.itemId },
