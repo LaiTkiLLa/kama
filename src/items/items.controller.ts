1310434d92acc687ab0dcd264285dcc9208b3db8
Fedor Burov <f.burov@kubis.ru>
2025-09-09 20:13:30 +0300

    Добавил запрос на получение списка товаров

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -5,6 +5,7 @@ import { UpdateItemInfoDto } from './dto/update-item-info.dto';
 import { GetItemsStopListDto } from './dto/get-items-stop-list.dto';
 import { UpdateStopListItems } from './dto/update-status-stop-list.dto';
 import { UpdateArrayDirectoryItemsInfoDto } from './dto/update-directory-item-info.dto';
+import { GetDirectoryListDto } from './dto/get-directory-list.dto';
 
 @Controller('items')
 export class ItemsController {
@@ -16,8 +17,11 @@ export class ItemsController {
   }
 
   @Get('directory/list')
-  async getItemsDirectoryList(@Headers('api-key') apiKey: string) {
-    return this.itemsService.getItemsDirectoryList();
+  async getItemsDirectoryList(
+    @Headers('api-key') apiKey: string,
+    @Query() getDirectoryListDto: GetDirectoryListDto
+  ) {
+    return this.itemsService.getItemsDirectoryList(getDirectoryListDto);
   }
 
   @Patch('directory/info')
