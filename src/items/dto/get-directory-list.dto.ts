38ab917cd19b5cc4b9bf6a159fb0833dff9ef7b6
Fedor Burov <f.burov@kubis.ru>
2025-09-09 20:21:10 +0300

    Добавил запрос на получение списка товаров

--- src/items/dto/get-directory-list.dto.ts
+++ src/items/dto/get-directory-list.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class GetDirectoryListDto {
   @IsString()
+  @IsOptional()
   supplierTitle: string
 }
