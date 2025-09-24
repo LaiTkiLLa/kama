de84962a7c5bd090b7599fc150bc24ac8bce441a
f.burov <f.burov@solber.ru>
2025-09-24 18:00:52 +0300

    добавил обновление картинок товаров

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -404,7 +404,8 @@ export class ItemsService {
     return queryRunner.manager.update(Items, where, updateData);
   }
 
-  @Cron('0 */40 * * * *')
+  @Cron(CronExpression.EVERY_MINUTE)
+  // @Cron('0 */40 * * * *')
   async getWbItems() {
     const itemsUrl = 'https://content-api.wildberries.ru/content/v2/get/cards/list';
     const apiToken = await this.configService.get('wbToken');
