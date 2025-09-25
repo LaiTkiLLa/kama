5db8601b124f427ec39d376a44825936376c1792
Fedor Burov <f.burov@kubis.ru>
2025-09-25 15:49:03 +0300

    Поправил скачивание товаров

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -404,8 +404,7 @@ export class ItemsService {
     return queryRunner.manager.update(Items, where, updateData);
   }
 
-  @Cron(CronExpression.EVERY_MINUTE)
-  // @Cron('0 */40 * * * *')
+  @Cron('0 */40 * * * *')
   async getWbItems() {
     const itemsUrl = 'https://content-api.wildberries.ru/content/v2/get/cards/list';
     const apiToken = await this.configService.get('wbToken');
@@ -442,7 +441,6 @@ export class ItemsService {
         nmID: data.cursor.nmID
       };
     }
-    console.log(items.length)
     const wbMarketplace = await this.infoService.findMarketplace({ title: 'WB' });
     for (const item of items) {
       const queryRunner = this.dataSource.createQueryRunner();
