9cb86dd70c8131e5d4a0636aad63f48f7cb47d06
f.burov <f.burov@solber.ru>
2026-01-27 15:08:03 +0300

    "https://kamabrand.bitrix24.ru/workgroups/group/93/tasks/task/view/2201/"

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 (async () => {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true
   });
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
   const PORT = configService.get('serverPort');
-  app.enableCors()
+  app.enableCors();
   await app.listen(PORT, () => {
     console.warn(`Server started on port: ${PORT}`);
   });
