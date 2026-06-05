fe8a87bae25683132aa1aec970f39570782aa625
f.burov <f.burov@solber.ru>
2026-06-05 16:56:42 +0300

    TASK-2927

--- database/migrations/1780663776255-add_column_orders_v2.ts
+++ database/migrations/1780663776255-add_column_orders_v2.ts
@@ -4,7 +4,7 @@ export class AddColumnOrdersV21780663776255 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumns('orders_v2', [
       new TableColumn({
-        type: 'timestampz',
+        type: 'timestamp',
         name: 'marketplace_created_at',
         isNullable: false
       })
