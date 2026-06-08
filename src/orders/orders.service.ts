b897292a77615880de4f72403b1abb40b8c73877
f.burov <f.burov@solber.ru>
2026-06-08 16:28:09 +0300

    TASK-2927

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -403,7 +403,7 @@ export class OrdersService {
     return;
   }
 
-  @Cron('0 */19 * * * *')
+  @Cron('0 45 * * * *')
   async getOrdersWbV2() {
     const tenDaysAgo = new Date(new Date().setDate(new Date().getDate() - 180));
     const apiToken = this.configService.get<string>('wbToken');
