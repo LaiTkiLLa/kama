8cc5a8e1b221868f0bb02bc641b09607aa531f8a
f.burov <f.burov@solber.ru>
2026-08-05 08:28:01 +0300

    TASK-2927

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -295,6 +295,9 @@ export class ItemsService {
             );
           }
         }
+        console.log('article', item.article);
+        console.log('findItems', findItems.map(el => el.id));
+        console.log('classification', item.classification);
         await queryRunner.manager.update(
           Items,
           { id: In(findItems.map(el => el.id)) },
