1d8d0aee7e1f4fff0f18369aae5213cacb01416f
f.burov <f.burov@solber.ru>
2026-08-25 11:57:21 +0300

    Добавил запрос на изменение данных по товару поставщика

---
+++ src/items/dto/update-erp-items-suppliers-list.dto.ts
@@ -0,0 +1,70 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested
+} from 'class-validator';
+
+export class UpdateErpItemSuppliersListDto {
+  @IsNumber()
+  itemSupplierId: number;
+
+  @IsString()
+  @IsOptional()
+  boxNumber: string;
+
+  @IsString()
+  @IsOptional()
+  multiplicity: string;
+
+  @IsNumber()
+  @IsOptional()
+  supplierMinimumOrder: number;
+
+  @IsNumber()
+  @IsOptional()
+  costInYuan: number;
+
+  @IsNumber()
+  @IsOptional()
+  costInYuanWhite: number;
+
+  @IsNumber()
+  @IsOptional()
+  payment: number;
+
+  @IsNumber()
+  @IsOptional()
+  production: number;
+
+  @IsNumber()
+  @IsOptional()
+  assembling: number;
+
+  @IsString()
+  @IsOptional()
+  dimensionsMasterBox: string;
+
+  @IsString()
+  @IsOptional()
+  dimensionsFact: string;
+
+  @IsString()
+  @IsOptional()
+  volume: string;
+}
+
+export class UpdateArrayErpItemsSuppliersListDto {
+  @ValidateNested({
+    each: true
+  })
+  @Type(() => UpdateErpItemSuppliersListDto)
+  @IsArray()
+  @IsNotEmpty()
+  @ArrayMinSize(1)
+  items: UpdateErpItemSuppliersListDto[];
+}
